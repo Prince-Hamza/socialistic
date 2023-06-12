c553dbc1f8180d57d16858cfacb4709f9fde7b18
Prince-Hamza <princehamzi.mine@gmail.com>
2023-06-12 19:15:00 +0500

    up

--- server/index.js
+++ server/index.js
@@ -19,7 +19,7 @@ import { fileURLToPath } from 'url';
 const __filename = fileURLToPath(import.meta.url)
 const __dirname = path.dirname(__filename)
 
-
+console.log(`server`)
 
 const app = express();
 const httpServer = createServer(app)
