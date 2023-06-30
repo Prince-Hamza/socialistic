a734ac8f094ce9058f8f57c51029370e6b56aea1
Prince-Hamza <princehamzi.mine@gmail.com>
2023-06-30 16:46:18 +0500

    so far

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -81,7 +81,6 @@ const Home = () => {
         <Col lg={5}> <PostSide /> </Col>
         <Col lg={3} ><RightSide /></Col>
       </Row>
-      {/* <GlobalSocketListener /> */}
     </Container>
   )
 }
